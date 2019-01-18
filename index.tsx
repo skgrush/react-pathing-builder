d0eb9ab9461d6bc53622b548d91b94aa9baa8ebd
Samuel K. Grush <Samuel@Grush.org>
2019-01-18 02:17:22 -0600

    Updates

--- index.tsx
+++ index.tsx
@@ -16,8 +16,8 @@ const DEFAULT =
 class App extends React.Component<any, State> {
   state: State = {imgSrc: null}
 
-  onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.debug('onChange file uploaded:', [e])
+  onUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.debug('onUpload file uploaded:', [e])
     if (e.currentTarget && e.currentTarget.value) {
       const curFiles = e.currentTarget.files
       if (curFiles && curFiles.length) {
@@ -28,15 +28,57 @@ class App extends React.Component<any, State> {
     }
   }
 
+  onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && e.currentTarget) {
+      e.persist()
+      console.debug('onKeyDown[Enter]:', e)
+      let {value} = e.currentTarget
+      if (!value) {
+        value = null
+      } else if (value.indexOf('://') === -1) {
+        value = `http://${value}`
+        e.currentTarget.value = value
+      }
+      this.setState({imgSrc: value})
+    }
+  }
+
   render() {
     console.log(this.state)
     return (
       <main>
-        <input ref="upload" type="file" onChange={this.onChange} />
+        <div>
+          <h2>
+            Demo of <code>react-pathing-builder</code> PathingBuilder Component
+          </h2>
+          <p>
+            The component requires the prop <code>mapSrc</code>, which can be an
+            image URL, an <code>HTMLImageElement</code>
+            object, or <code>null</code>.
+          </p>
+          <p>
+            In the following example, you can set the <code>mapSrc</code>
+            prop with a uploaded image, an entered URL, or with a default image
+            URL.
+          </p>
+        </div>
+        <label htmlFor="upload">Upload:</label>
+        <input name="upload" type="file" onChange={this.onUpload} />
+        <label htmlFor="url">URL:</label>
+        <input
+          type="url"
+          name="url"
+          placeholder="https://example.com"
+          pattern="https://.*"
+          onKeyDown={this.onKeyDown}
+        />
+        <label htmlFor="default">Default:</label>
         <input
           type="button"
+          name="default"
           onClick={() => this.setState({imgSrc: DEFAULT})}
           value="Default Img"
+          title={DEFAULT}
         />
         {this.state.imgSrc && (
           <PathingBuilder
